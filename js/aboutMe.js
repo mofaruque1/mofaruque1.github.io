ae46397dcca5cdfe85b0e0d6cc01e5a46b5d71e2
Md Omor Faruque <mofaruque1@gmail.com>
2017-12-28 21:58:13 -0500

    Contact Page Added v1

---
+++ js/aboutMe.js
@@ -0,0 +1,4 @@
+$('document').ready(function(){
+    $('div.about-header-img').animate({ opacity: 1 }, { duration: 3000 });
+   
+});
---
+++ js/contact.js
@@ -0,0 +1,4 @@
+$('document').ready(function(){
+    $('div.contact-header-img').animate({ opacity: 1 }, { duration: 3000 });
+   
+});
