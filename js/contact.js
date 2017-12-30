cd42e52b2118805d12ce2211f7ff62cc320ecfe4
Md Omor Faruque <mofaruque1@gmail.com>
2017-12-29 21:33:41 -0500

    fixed the code page v1

--- js/contact.js
+++ js/contact.js
@@ -1,4 +1,5 @@
 $('document').ready(function(){
     $('div.contact-header-img').animate({ opacity: 1 }, { duration: 3000 });
+    $('div.codes-header-img').animate({ opacity: 1 }, { duration: 3000 });
    
 });
