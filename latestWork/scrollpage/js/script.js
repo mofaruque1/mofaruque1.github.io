78db9fcecf3d7e9846e425a4ce2779812715bba5
Md Omor Faruque <mofaruque1@gmail.com>
2018-03-22 07:10:22 -0400

    Added the scroll animation page v12

--- latestWork/scrollpage/js/script.js
+++ latestWork/scrollpage/js/script.js
@@ -1,1 +1,79 @@
-window.sr=ScrollReveal(),sr.reveal(".navbar",{duration:2e3,origin:"bottom"}),sr.reveal("#showcase .showcase-left",{duration:2e3,origin:"top",distance:"200px"}),sr.reveal("#showcase .showcase-right",{duration:2e3,distance:"200px",origin:"right"}),sr.reveal(".showcase-btn",{duration:2e3,delay:1e3,origin:"bottom"}),sr.reveal("#testimonial div",{duration:2e3,origin:"bottom"}),sr.reveal(".info-left",{duration:2e3,distance:"250px",origin:"left",viewFactor:.2}),sr.reveal(".info-right",{duration:2e3,distance:"250px",origin:"right",viewFactor:.2}),sr.reveal(".contact-showcase",{duration:1500,distance:"250px",delay:500,origin:"left",viewFactor:.2}),sr.reveal(".img-responsive",{duration:1500,distance:"100px",delay:500,origin:"right",viewFactor:.2}),$(function(){$('a[href*="#"]:not([href="#"])').click(function(){if(location.pathname.replace(/^\//,"")==this.pathname.replace(/^\//,"")&&location.hostname==this.hostname){var e=$(this.hash);if((e=e.length?e:$("[name="+this.hash.slice(1)+"]")).length)return $("html, body").animate({scrollTop:e.offset().top},1e3),!1}})});
+window.sr = ScrollReveal();
+
+sr.reveal('.navbar',{
+    duration:2000,
+    origin:'bottom'
+});
+
+sr.reveal('#showcase .showcase-left',{
+    duration:2000,
+    origin:'top',
+    distance:'200px'
+});
+
+sr.reveal('#showcase .showcase-right',{
+    duration:2000,
+    distance:'200px',
+    origin:'right'
+});
+
+
+sr.reveal('.showcase-btn',{
+    duration:2000,
+    delay:1500,
+    origin:'bottom'
+});
+
+sr.reveal('#testimonial div', {
+    duration: 2000,
+    origin:'bottom'
+});
+
+
+sr.reveal('.info-left', {
+    duration: 2000,
+    distance:'250px',
+    origin:'left',
+    viewFactor: 0.2
+});
+
+sr.reveal('.info-right', {
+    duration: 2000,
+    distance:'250px',
+    origin:'right',
+    viewFactor: 0.2
+});
+
+sr.reveal('.contact-showcase', {
+    duration: 2000,
+    distance:'250px',
+    delay:1000,
+    origin:'left',
+    viewFactor: 0.3
+});
+
+
+
+sr.reveal('.img-responsive', {
+    duration: 2000,
+    distance:'100px',
+    delay:500,
+    origin:'right',
+    viewFactor: 0.3
+});
+
+$(function() {
+    // Smooth Scrolling
+    $('a[href*="#"]:not([href="#"])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
+      }
+    });
+  });
